fe7e007dc99782175965ac9569d7ae718602b91f
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-09-12 14:52:09 +0300

    Make migrations and migrate

---
+++ database/migrations/2023_09_12_144906_create_topics_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->increments('topic_id');
+            $table->string('topic_subject', 255);
+            $table->dateTime('topic_date');
+            $table->integer('topic_cat')->unsigned();
+            $table->integer('topic_by')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('topics');
+    }
+};
---
+++ database/migrations/2023_09_12_144919_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('post_id');
+            $table->text('post_content');
+            $table->dateTime('post_date');
+            $table->integer('post_topic')->unsigned();
+            $table->integer('post_by')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
